cc4031b5039f71409f028e5cff5dce2d15183457
Michael Nevzorov <mvnevzorov@gmail.com>
2019-12-22 21:39:19 +0300

    Made Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        <span className="header__title_poke">POKE</span>
+        <span className="header__title_dex">DEX</span>
+      </h1>
+    </header>
+  )
+}
+
+export default Header
