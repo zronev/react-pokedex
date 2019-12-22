7b7524e84c88b623182b777d856ba6bdb7a3dff2
Michael Nevzorov <mvnevzorov@gmail.com>
2019-12-22 21:40:21 +0300

    Added classes for TypesItem

--- src/components/TypesItem.js
+++ src/components/TypesItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const TypesItem = ({ type }) => {
-  return <li>{type.name}</li>
+  return <li className={`types__type types__type_${type.name}`}>{type.name}</li>
 }
 
 export default TypesItem
