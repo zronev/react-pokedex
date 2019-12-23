da8de741a25f835d80330888efafc66a07bdaf88
Michael Nevzorov <mvnevzorov@gmail.com>
2019-12-23 16:48:14 +0300

    Added speciesReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,10 +5,12 @@ import { chooseChartTypeReducer } from './chooseChartTypeReducer'
 
 const pokemons = getAsyncDataReducer('pokemons')
 const pokemon = getAsyncDataReducer('pokemon')
+const species = getAsyncDataReducer('species')
 
 export const rootReducer = combineReducers({
   pokemons,
   pokemon,
+  species,
   id: pokemonIDReducer,
   chartType: chooseChartTypeReducer,
 })
