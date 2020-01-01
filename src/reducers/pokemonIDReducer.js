317bad8748053ddc0d55c51a9f0972b81bfa5be4
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-01 19:23:27 +0300

    Added 'SET_ID' case

--- src/reducers/pokemonIDReducer.js
+++ src/reducers/pokemonIDReducer.js
@@ -4,6 +4,8 @@ export const pokemonIDReducer = (state = 1, action) => {
       return state !== 802 ? state + 1 : 1
     case 'PREV_POKEMON':
       return state !== 1 ? state - 1 : 802
+    case 'SET_ID':
+      return action.payload
     default:
       return state
   }
