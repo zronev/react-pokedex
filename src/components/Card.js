8171fedfcc6cf30addc9fcf17177d0c75ea930fe
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-03 18:43:34 +0300

    Made Card component

---
+++ src/components/Card.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import TypesList from './TypesList'
+import AbilitiesList from './AbilitiesList'
+import Sprite from './Sprite'
+import Spinner from './Spinner'
+
+const Card = ({ url }) => {
+  const [pokemon, setPokemon] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url)
+      const data = await res.json()
+
+      setPokemon(data)
+    }
+
+    fetchData()
+  }, [url])
+
+  return (
+    <section className="card">
+      {pokemon ? (
+        <>
+          <div className="card__header">
+            <p className="name card__name">
+              <span className="id card__id">{pokemon.id}</span>
+              {` ${pokemon.name}`}
+            </p>
+
+            <button className="like">
+              <i className="far fa-heart" />
+            </button>
+          </div>
+
+          {pokemon.sprites ? (
+            <Sprite sprites={pokemon.sprites} parent={'card'} />
+          ) : (
+            <Spinner />
+          )}
+
+          <TypesList types={pokemon.types} />
+          <AbilitiesList abilities={pokemon.abilities} />
+        </>
+      ) : null}
+    </section>
+  )
+}
+
+export default Card
