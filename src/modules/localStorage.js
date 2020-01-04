174a36a7edbca9ba24fc45fb2b425750347f3608
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-04 23:12:47 +0300

    Added localStorage

---
+++ src/modules/localStorage.js
@@ -0,0 +1,22 @@
+export const saveToLocalStorage = state => {
+  // Using try/catch because of different privacy
+  // settings from one user to another
+
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) return undefined
+    return JSON.parse(serializedState)
+  } catch (e) {
+    console.log(e)
+    return undefined
+  }
+}
--- src/store.js
+++ src/store.js
@@ -1,8 +1,20 @@
 import { createStore } from 'redux'
 import { rootReducer } from './reducers'
+import { saveToLocalStorage, loadFromLocalStorage } from './modules/localStorage'
+
+const persistedState = loadFromLocalStorage()
+if (persistedState !== undefined) {
+  persistedState.id = 1
+  persistedState.offset = 0
+  persistedState.side = false
+  persistedState.menu = false
+}
 
 export const store = createStore(
   rootReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__({ serialize: true, trace: true }),
 )
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
