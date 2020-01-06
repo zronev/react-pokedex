cc9c6565f3d0905591fdcc519259de8f5a907393
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-06 19:31:36 +0300

    Added correct basename for Router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import About from './components/About'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/react-pokedex">
       <div className="App">
         <Header />
         <Menu />
