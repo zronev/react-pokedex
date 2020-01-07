acc3373fc7f92206f9dee59c923a890705902afc
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-07 19:22:06 +0300

    Small change

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <header className="header">
       <h1 className="header__title">
-        <span className="header__title_poke">Poke</span>
+        <span className="header__title_poke">Poké</span>
         <span className="header__title_dex">dex</span>
       </h1>
       <Pokeball />
