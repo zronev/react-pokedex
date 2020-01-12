d1faffb2e22d18177254713688bd0713e1567fc6
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-12 12:58:17 +0300

    Added image of pokemon above its name

--- src/components/EvolutionItem.js
+++ src/components/EvolutionItem.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from 'react'
 
 const EvolutionItem = ({ name }) => {
-  return <li className="evolution evolutions__evolution">{name}</li>;
-};
+  const [formattedName, setFormattedName] = useState('')
+  const [pokemon, setPokemon] = useState({})
 
-export default EvolutionItem;
+  const formatName = useCallback(() => {
+    if (!name.includes('>')) {
+      setFormattedName(name)
+      return
+    }
+    setFormattedName(name.slice(2, name.length))
+  }, [name])
+
+  const fetchData = useCallback(async () => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${formattedName}/`)
+    const data = await res.json()
+
+    setPokemon(data)
+  }, [formattedName])
+
+  useEffect(() => {
+    formatName()
+
+    if (formattedName) fetchData()
+  }, [fetchData, formatName, formattedName, name])
+
+  return (
+    <li className="evolution evolutions__evolution">
+      {pokemon.sprites && <img src={pokemon.sprites.front_default} alt="" />}
+      <span>{name}</span>
+    </li>
+  )
+}
+
+export default EvolutionItem
