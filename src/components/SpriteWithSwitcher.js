a2606f09107868b352c63e0e0570c2f35fc2880e
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-17 21:21:05 +0300

    Added rotate animation

--- src/components/SpriteWithSwitcher.js
+++ src/components/SpriteWithSwitcher.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { dataToggleAction } from '../actions'
 import Sprite from './Sprite'
@@ -7,16 +7,23 @@ const SpriteWithSwitcher = ({ sprites }) => {
   const dispatch = useDispatch()
   const side = useSelector(state => state.side)
 
+  const [rotate, setRotate] = useState(false)
+
   const handleClick = () => {
     dispatch(dataToggleAction('side'))
+    setRotate(true)
   }
 
   return (
     <div className="sprite pokemon__sprite">
       <Sprite side={side} sprites={sprites} />
-      <button onClick={handleClick} className="button button--switch sprite__switch-side">
-        <i className="fas fa-sync-alt"></i>
-      </button>
+      <i
+        onClick={handleClick}
+        onAnimationEnd={() => setRotate(false)}
+        className={`sprite__switch-side ${
+          rotate ? 'sprite__switch-side--active' : ''
+        } fas fa-sync-alt`}
+      />
     </div>
   )
 }
