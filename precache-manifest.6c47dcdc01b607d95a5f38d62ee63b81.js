66859815dfff45ee62e4609aeec07ffb3ed43523
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-18 17:22:57 +0300

    Updates

---
+++ precache-manifest.6c47dcdc01b607d95a5f38d62ee63b81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbea3b266a4c246a12163e74d03aa7f",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "463160e3c341f1adb02d",
+    "url": "/react-pokedex/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ecacd06b66012fc9489c",
+    "url": "/react-pokedex/static/css/main.964f8365.chunk.css"
+  },
+  {
+    "revision": "463160e3c341f1adb02d",
+    "url": "/react-pokedex/static/js/2.e244c38a.chunk.js"
+  },
+  {
+    "revision": "44bb13c376a360a26522c8368cf779a1",
+    "url": "/react-pokedex/static/js/2.e244c38a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecacd06b66012fc9489c",
+    "url": "/react-pokedex/static/js/main.8591d2e3.chunk.js"
+  },
+  {
+    "revision": "ebecc74468765ea72f07",
+    "url": "/react-pokedex/static/js/runtime-main.f71a6969.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-pokedex/precache-manifest.2a2d9a31a0077eba349eefa45fcdc3e7.js"
+  "/react-pokedex/precache-manifest.6c47dcdc01b607d95a5f38d62ee63b81.js"
 );
 
 self.addEventListener('message', (event) => {
