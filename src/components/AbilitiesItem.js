6e2d4b36f60f4f45d2225d37770d9b842f47ebcf
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-19 15:21:59 +0300

    Info opens in full-size window on small screen

--- src/components/AbilitiesItem.js
+++ src/components/AbilitiesItem.js
@@ -4,6 +4,16 @@ const AbilitiesItem = ({ ability }) => {
   const [showDetail, setShowDetail] = useState(false)
   const [abilityData, setAbilityData] = useState({})
 
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const updateSize = () => setScreenWidth(window.innerWidth)
+    updateSize()
+
+    window.addEventListener('resize', updateSize)
+    return () => window.removeEventListener('resize', updateSize)
+  }, [screenWidth])
+
   useEffect(() => {
     const controller = new AbortController()
     const fetchData = async () => {
@@ -20,19 +30,44 @@ const AbilitiesItem = ({ ability }) => {
     return () => controller.abort()
   }, [ability.url])
 
+  const handleClick = () => {
+    if (screenWidth >= 1024) return
+    setShowDetail(true)
+  }
+  const handleCloseClick = () => {
+    if (screenWidth >= 1024) return
+    setShowDetail(false)
+  }
+
+
+  const handleMouseEnter = () => {
+    if (screenWidth < 1024) return
+    setShowDetail(true)
+  }
+
+  const handleMouseLeave = () => {
+    if (screenWidth < 1024) return
+    setShowDetail(false)
+  }
+
   return (
-    <li
-      onMouseEnter={() => setShowDetail(true)}
-      onMouseLeave={() => setShowDetail(false)}
-      className="abilty abilities__ability"
-    >
-      <span className="ability__name">{ability.name}</span>
-      {showDetail && (
-        <p className="ability__descr">
-          {abilityData.effect_entries && abilityData.effect_entries[0].short_effect}
-        </p>
-      )}
-    </li>
+    <>
+      <li
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={handleClick}
+        className="abilty abilities__ability"
+      >
+        <span className="ability__name">{ability.name}</span>
+        {showDetail && (
+          <p className="ability__descr">
+            {abilityData.effect_entries && abilityData.effect_entries[0].short_effect}
+          </p>
+        )}
+      </li>
+
+      {screenWidth < 1024 && <div onClick={handleCloseClick} className={`${showDetail ? 'overlay' : ''}`} />}
+    </>
   )
 }
 
