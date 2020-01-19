e301db344b3d577d8a17ae96dbfd833bea3c509c
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-19 19:07:38 +0300

    Temporary solution

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
 import { incLimit, loadPokemons } from '../actions'
@@ -15,12 +15,10 @@ const Grid = () => {
   const pokemons = useSelector(state => state.pokemons)
 
   const [pokemonsData, setPokemonsData] = useState()
-  const [isVisible, setIsVisible] = useState(false)
-  // const [isFetching, setIsFetching] = useState(false)
+  // const [isVisible, setIsVisible] = useState(false)
 
   const handleClick = () => {
-    setIsVisible(false)
-    // setIsFetching(true)
+    // setIsVisible(false)
     dispatch(incLimit())
   }
 
@@ -33,12 +31,10 @@ const Grid = () => {
 
   useEffect(() => {
     setPokemonsData(pokemons.data.results)
-    // setIsFetching(false)
   }, [pokemons])
 
   const liked = useSelector(state => state.liked.sort((a, b) => a - b))
   const showLiked = useSelector(state => state.showLiked)
-
   const isLoaded = !!pokemonsData
 
   const loadAll = isLoaded
@@ -46,23 +42,24 @@ const Grid = () => {
     : null
 
   const loadLiked = isLoaded
-    ? liked.map((url, index) => (
-        <Card key={index} url={`https://pokeapi.co/api/v2/pokemon/${url}`} />
+    ? liked.map((id, index) => (
+        <Card key={index} url={`https://pokeapi.co/api/v2/pokemon/${id}`} />
       ))
     : null
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const fullLength = window.innerHeight + document.documentElement.scrollTop
-      if (fullLength !== document.documentElement.offsetHeight || showLiked) return
-      setIsVisible(true)
-    }
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom
+  //     let closeEnoughToBottom =
+  //       windowRelativeBottom < document.documentElement.clientHeight + 150
+  //     setScreenHeight(window.screen.height)
 
-    document.addEventListener('scroll', handleScroll, false)
-    return () => {
-      document.removeEventListener('scroll', handleScroll, false)
-    }
-  }, [dispatch, showLiked])
+  //     closeEnoughToBottom && setIsVisible(true)
+  //   }
+
+  //   document.addEventListener('scroll', handleScroll, false)
+  //   return () => document.removeEventListener('scroll', handleScroll, false)
+  // }, [dispatch, showLiked])
 
   return (
     <div className="grid">
@@ -72,12 +69,12 @@ const Grid = () => {
         {pokemons.error && !loadLiked && !loadAll && <p>Load Error</p>}
         {pokemons.loading && <Spinner />}
       </main>
-      {isVisible && !showLiked && (
+      {!showLiked && (
         <button onClick={handleClick} className="button button--show-more grid__button">
           Show More
         </button>
       )}
-      {/* {isFetching && <Spinner />} */}
+      {pokemons.loading && <Spinner />}
     </div>
   )
 }
