018da40751604ff0b1c0094807b4f02f67137161
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-29 12:40:27 +0300

    Updates

---
+++ precache-manifest.c879d4b504b2f63e6fef83bd8405dde0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01d713ea7b115d497652921385439f9",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "8b46ee964e8bd3e44c92",
+    "url": "/react-pokedex/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9f4555e48fff31e4072e",
+    "url": "/react-pokedex/static/css/main.dcc3283d.chunk.css"
+  },
+  {
+    "revision": "8b46ee964e8bd3e44c92",
+    "url": "/react-pokedex/static/js/2.2b1cb068.chunk.js"
+  },
+  {
+    "revision": "44bb13c376a360a26522c8368cf779a1",
+    "url": "/react-pokedex/static/js/2.2b1cb068.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f4555e48fff31e4072e",
+    "url": "/react-pokedex/static/js/main.5cd12696.chunk.js"
+  },
+  {
+    "revision": "ebecc74468765ea72f07",
+    "url": "/react-pokedex/static/js/runtime-main.f71a6969.js"
+  }
+]);
