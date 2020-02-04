040ca9abc084154f4a62d4a21b366a28394c2515
Michael Nevzorov <mvnevzorov@gmail.com>
2020-02-04 14:01:28 +0300

    Made GoToCompare component

---
+++ src/components/GoToCompare.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const GoToCompare = () => {
+  const toCompare = useSelector(state => state.toCompare)
+  
+  return (
+    <Link className="button go-to-compare" to="/compare">
+      Go to compare {toCompare.length} / 6
+    </Link>
+  )
+}
+
+export default GoToCompare
